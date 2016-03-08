5752ec672e9d1ca239d686eba5b9f4723cb6b643
Albert Segarra Roca <albertsgrc@gmail.com>
2016-03-08 08:55:55 +0100

    Project and file structure

---
+++ src/DistribFileSystemGoalTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by albert on 08/03/16.
+ */
+import aima.search.framework.GoalTest;
+
+public class DistribFileSystemGoalTest implements GoalTest {
+    @Override
+    public boolean isGoalState(Object state) {
+        return false;
+    }
+}
